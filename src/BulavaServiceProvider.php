b14a32c26557a591595fe0c1921e7ea3cbb1e093
Maksym Maksymenko <6aff21@gmail.com>
2019-07-02 13:48:57 +0300

    first commit

---
+++ src/BulavaServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Makabama\Bulava;
+
+use Illuminate\Support\ServiceProvider;
+
+class BulavaServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
